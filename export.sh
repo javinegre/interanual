abfc3a43220314d2313c43382c15b4d621e26d05
Javier Negre <javiscript.me@gmail.com>
2015-03-18 23:53:13 +0100

    First export to ./data/interanual.csv

--- export.sh
+++ export.sh
@@ -4,4 +4,4 @@ DATABASE=interanual
 COLLECTION=historical
 FIELDS=periodo,total,hombres,mujeres,hombresMenor25,mujeresMenor25,totalMenor25,hombresMayor25,mujeresMayor25,totalMayor25,agricultura,industria,construccion,servicios,sinEmpleo,andalucia,aragon,asturias,balears,canarias,cantabria,castillaMancha,castillaLeon,catalunya,comValenciana,extremadura,galicia,madrid,murcia,navarra,paisVasco,rioja,ceuta,melilla,ts
 
-mongoexport -d $DATABASE -c $COLLECTION --csv -f $FIELDS -q '{ $query: {}, $orderby: {ts: 1} }'
+mongoexport -d $DATABASE -c $COLLECTION --csv -f $FIELDS -q '{ $query: {}, $orderby: {ts: -1} }' > ./data/interanual.csv
