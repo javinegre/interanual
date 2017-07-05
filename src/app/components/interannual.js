d5309e487d2e2c7897d0096392e13704c8d71ed8
Javier Negre <javi.negre@gmail.com>
2017-07-05 10:30:06 +0200

    Refactor index.js

---
+++ src/app/components/interannual.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import _ from 'lodash';
+
+import IaChart from './ia-chart';
+import DataTable from './data-table';
+import ModeSelector from './mode-selector';
+import MetricSelector from './metric-selector';
+
+import metricList from '../data/metrics';
+
+export default class Interanual extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      metrics: ['total'],
+      mode: 'interannual',
+      tableData: null,
+      metricColors: {}
+    };
+  }
+
+  updateMode (mode) {
+    this.updateMetrics(null, mode);
+  }
+
+  updateMetrics (metric, mode) {
+    const newMetric = metric || _.head(this.state.metrics);
+    const newMode = mode || this.state.mode;
+
+    let newMetrics;
+
+    if (newMode === 'interannual') {
+      newMetrics = [newMetric];
+    } else {
+      newMetrics = _.xor(this.state.metrics, [newMetric]);
+
+      if (newMetrics.length === 0) {
+        newMetrics = [_.nth(metricList, 0).key, _.nth(metricList, 1).key];
+      }
+      else if (newMetrics.length === 1 && _.head(newMetrics) == _.nth(metricList, 0).key) {
+        newMetrics.push(_.nth(metricList, 1).key);
+      }
+      else if (newMetrics.length === 1) {
+        newMetrics.push(_.nth(metricList, 0).key);
+      }
+    }
+
+    this.setState({
+      mode: newMode,
+      metrics: newMetrics
+    });
+  }
+
+  onChartLoad (data) {
+    // Wait for all components to render
+    window.setTimeout(() => { this.setState({ metricColors: data.colors }); }, 5);
+  }
+
+  onDataReceived (data) {
+    // Wait for all components to render
+    window.setTimeout(() => { this.setState({ tableData: data }); }, 10);
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="block full-width clearfix">
+
+          <DataTable
+            metrics={this.state.metrics}
+            data={this.state.tableData} />
+
+          <IaChart
+            metrics={this.state.metrics}
+            onLoad={this.onChartLoad.bind(this)}
+            onDataReceived={this.onDataReceived.bind(this)} />
+
+        </div>
+
+        <div className="chart-options block clearfix">
+
+          <ModeSelector
+            mode={this.state.mode}
+            onChange={this.updateMode.bind(this)} />
+
+          <MetricSelector
+            metrics={this.state.metrics}
+            metricColors={this.state.metricColors}
+            onChange={this.updateMetrics.bind(this)} />
+
+        </div>
+      </div>
+    )
+  }
+}
