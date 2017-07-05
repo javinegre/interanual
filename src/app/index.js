f86d3a263b5fac750338fbeb5c8f0050fb822b3b
Javier Negre <javi.negre@gmail.com>
2017-07-05 18:09:03 +0200

    Localize name of months

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 
 import Interannual from './components/interannual';
 
 import '../styles/style.scss';
 
-ReactDOM.render(<Interannual />, document.getElementById('chart'));
+moment.locale('es', {
+  months : [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+    'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+  ]
+});
+moment.locale('es');
+
+const App = <Interannual />;
+
+ReactDOM.render(App, document.getElementById('chart'));
