59c9a9a478dd91506a898b9f8a87044903a805dd
Javier Negre <javi.negre@gmail.com>
2017-07-05 18:43:09 +0200

    Move export destination to another folder

--- scripts/export.sh
+++ scripts/export.sh
@@ -3,6 +3,6 @@
 DATABASE=interanual
 COLLECTION=historical
 FIELDS=periodo,total,hombres,mujeres,hombresMenor25,mujeresMenor25,totalMenor25,hombresMayor25,mujeresMayor25,totalMayor25,agricultura,industria,construccion,servicios,sinEmpleo,andalucia,aragon,asturias,balears,canarias,cantabria,castillaMancha,castillaLeon,catalunya,comValenciana,extremadura,galicia,madrid,murcia,navarra,paisVasco,rioja,ceuta,melilla,ts
-OFILE=./app/public/data/interanual.csv
+OFILE=./src/static/data/interanual.csv
 
 mongoexport -d $DATABASE -c $COLLECTION --type=csv -f $FIELDS -q '{}' --sort '{ts: -1}' > $OFILE
