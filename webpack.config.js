68a6d84952320e1c4ba4eb3d83be6bfdedf96d67
Javier Negre <javi.negre@gmail.com>
2017-07-06 15:50:43 +0200

    Proper caching of assets (by adding hash to the name)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
+const webpack = require('webpack');
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const VENDOR_LIBS = ['d3', 'lodash', 'moment', 'react', 'react-dom'];
+
 module.exports = {
-  entry: path.resolve(__dirname, 'src/app/index.js'),
+  entry: {
+    bundle: path.resolve(__dirname, 'src/app/index.js'),
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'build.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -29,7 +35,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html')
     }),
-    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new ExtractTextPlugin('style.[contenthash].css'),
     new CopyWebpackPlugin([{
         context: './src/static/',
         from: './data/*'
